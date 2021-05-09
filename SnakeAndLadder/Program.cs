493345d60a0eb0f4bea796d4279618ca13667058
shivam satpute <shivam897satpute@gmail.com>
2021-05-09 23:38:46 +0530

    [shivam] add: Snake and Ladder Game Start Player 0 Position

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{ 
+    class SnakeAndLadder
+    {
+        int START_POSITION = 0;
+        public void PlayGame()
+        {
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            SnakeAndLadder StartPlay = new SnakeAndLadder();
+            StartPlay.PlayGame();
+
+        }
+    }
+}
