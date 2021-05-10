f800229367894f52f9b68be7dd0d9e64b8b1c364
shivam satpute <shivam897satpute@gmail.com>
2021-05-11 00:42:22 +0530

    [shivam] add: Player rolls the die to get numbers 1 to 6

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,6 +11,9 @@ namespace SnakeAndLadder
         int START_POSITION = 0;
         public void PlayGame()
         {
+            Random random = new Random();
+            int Die = random.Next(1, 6);    
+            Console.WriteLine(" Player Die Roll Position :-" + Die);
             Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);
             Console.ReadLine();
         }
