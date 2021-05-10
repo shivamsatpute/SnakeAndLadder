eae8a201a0fbb25e331a7b926f26d56cf759d280
shivam satpute <shivam897satpute@gmail.com>
2021-05-11 01:31:10 +0530

    [shivam]add: Number of times the dice was played to win the game

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,38 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace SnakeAndLadder
 {  
     class SnakeAndLadder
     {
-        int START_POSITION = 0;
+        int START_POSITION = 0; 
         const int NoPlay = 0;
         const int Ladder = 1;
         const int Snakle = 2;
-        const int winning_position = 100;
+        const int winning_position = 100; 
         public void PlayGame()
         {
+            int Dice_Count = 0;
             int Player = 0;
             while (Player < winning_position)
             {
                 Random random = new Random(); 
                 int Die = random.Next(1, 7);    
                 int Option = random.Next(0, 3); 
+                Dice_Count++; 
+                Console.WriteLine($"Report the number of times the dice was played to win {Dice_Count}");
 
 
-                switch (Option)
+                switch (Option) 
                 {
                     case NoPlay:
-                        Console.WriteLine("No Play, Pass the chance"); 
+                        Console.WriteLine("No Play, Pass the chance");  
                         break;
                     case Ladder:
-                        Player += Die; 
-                        Console.WriteLine("You got Ladder \n Player position = " + Player); 
+                        Player += Die;
+                        Console.WriteLine("You got Ladder \n Player position = " + Player);
                         break;
                     case Snakle:
                         Player -= Die;
-                        if (Player < START_POSITION)
+                        if (Player < START_POSITION) 
                         {
                             Player = START_POSITION; 
                         }
@@ -45,14 +47,14 @@ namespace SnakeAndLadder
                 }
 
             }
-                             
-            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);  
+            Console.WriteLine($"Position After every Dice role {Dice_Count}");
             Console.ReadLine();
         }
         static void Main(string[] args)
         {
-            SnakeAndLadder StartPlay = new SnakeAndLadder();
+            SnakeAndLadder StartPlay = new SnakeAndLadder(); 
             StartPlay.PlayGame();
+
         }
     }
 }
