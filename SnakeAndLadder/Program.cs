ddd50b61a5bddb026f695d34af6ef59376c2d03d
shivam satpute <shivam897satpute@gmail.com>
2021-05-11 01:22:37 +0530

    [shivam]add: Ensure the player gets To Exact Winning position 100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,47 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SnakeAndLadder
 {  
     class SnakeAndLadder
     {
-        int START_POSITION = 0; 
+        int START_POSITION = 0;
         const int NoPlay = 0;
         const int Ladder = 1;
         const int Snakle = 2;
+        const int winning_position = 100;
         public void PlayGame()
         {
-            Random random = new Random(); 
-            int Die = random.Next(1, 6);    
-            int Option = random.Next(0, 3); 
             int Player = 0;
-            switch (Option)
+            while (Player < winning_position)
             {
-                case NoPlay:
-                    Console.WriteLine("No Play, Pass the chance"); 
-                    break;
-                case Ladder:
-                    Player = Die; // 
-                    Console.WriteLine("You got Ladder \n Player position = " + Player); 
-                    break;
-                case Snakle:
-                    Player -= Die;
-                    if (Player < START_POSITION) 
-                    {
-                        Player = START_POSITION; 
-                    }
-                    Console.WriteLine("You gotSnake \n player position = " + Player);
+                Random random = new Random(); 
+                int Die = random.Next(1, 7);    
+                int Option = random.Next(0, 3); 
 
-                    break;
+
+                switch (Option)
+                {
+                    case NoPlay:
+                        Console.WriteLine("No Play, Pass the chance"); 
+                        break;
+                    case Ladder:
+                        Player += Die; 
+                        Console.WriteLine("You got Ladder \n Player position = " + Player); 
+                        break;
+                    case Snakle:
+                        Player -= Die;
+                        if (Player < START_POSITION)
+                        {
+                            Player = START_POSITION; 
+                        }
+                        Console.WriteLine("You gotSnake \n player position = " + Player);
+
+                        break;
+                }
 
             }
-            Console.WriteLine(" Player Die Roll Position :- " + Die);                   
-            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);     
+                             
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);  
             Console.ReadLine();
         }
         static void Main(string[] args)
         {
-            SnakeAndLadder StartPlay = new SnakeAndLadder(); 
+            SnakeAndLadder StartPlay = new SnakeAndLadder();
             StartPlay.PlayGame();
         }
     }
