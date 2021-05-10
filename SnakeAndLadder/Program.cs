51ad04e41db02a05066a3f873b1d7d56bd81820b
shivam satpute <shivam897satpute@gmail.com>
2021-05-11 00:57:29 +0530

    [shivam] add: Snake and Ladder Game checks For a Option

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -8,18 +8,37 @@ namespace SnakeAndLadder
 { 
     class SnakeAndLadder
     {
-        int START_POSITION = 0;
+        int START_POSITION = 0; 
+        const int NoPlay = 0;
+        const int Ladder = 1;
+        const int Snakle = 2;
         public void PlayGame()
         {
-            Random random = new Random();
+            Random random = new Random(); 
             int Die = random.Next(1, 7);    
-            Console.WriteLine(" Player Die Roll Position : " + Die);
-            Console.WriteLine(" Single Player Start_Position:  " + START_POSITION);
+            int Option = random.Next(0, 3);
+            int Player = 0;
+            switch (Option) 
+            {
+                case NoPlay:
+                    Console.WriteLine("No Play, Pass the chance"); 
+                    break;
+                case Ladder:
+                    Player = Die; // 
+                    Console.WriteLine("You got Ladder \n Player position = " + Player);
+                    break;
+                case Snakle:
+                    Player = Die;
+                    Console.WriteLine("You gotSnake \n player position = " + Player); 
+                    break;
+            }
+            Console.WriteLine(" Player Die Roll Position :- " + Die);                  
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);     
             Console.ReadLine();
         }
-        static void Main(string[] args)
+        static void Main(string[] args)//Main method
         {
-            SnakeAndLadder StartPlay = new SnakeAndLadder();
+            SnakeAndLadder StartPlay = new SnakeAndLadder(); 
             StartPlay.PlayGame();
 
         }
